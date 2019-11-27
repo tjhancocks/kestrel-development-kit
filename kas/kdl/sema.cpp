9c62a6067ae0ff95dcd47c83bd1cf66d562f26af
Tom Hancocks <tomhancocks@gmail.com>
2019-11-27 07:19:47 +0000

    Flesh out the workings of the Semantic Analyzer.
    This is probably not how it should or will look, but its getting something
    together that works for now.

--- kas/kdl/sema.cpp
+++ kas/kdl/sema.cpp
@@ -21,11 +21,159 @@
 */
 
 #include "kdl/sema.hpp"
+#include <iostream>
 
 // MARK: - Constructor
 
 kdl::sema::sema(const std::vector<kdl::lexer::token> tokens)
-    : m_tokens(tokens)
+    : m_tokens(tokens), m_ptr(0)
 {
     
+}
+
+// MARK: - Semantic Analysis
+
+void kdl::sema::run()
+{
+    // Reset the iterator, in the event of running the semantic analysis twice we
+    // need to do this.
+    m_ptr = 0;
+    
+    while (!finished()) {
+        
+        if (expect(condition(kdl::lexer::token::type::directive).truthy(), condition(kdl::lexer::token::type::lbrace).truthy())) {
+            // We're looking at a directive - determine which directive, and advance past the directive and l-brace.
+            auto directive = peek().text();
+            advance(2);
+            
+            if (directive == "out") {
+                // Consume tokens until a r-brace is encountered.
+                auto tokens = consume(condition(kdl::lexer::token::type::rbrace).falsey());
+                for (auto tk: tokens) {
+                    std::cout << tk.text() << std::endl;
+                }
+            }
+            else {
+                throw std::runtime_error("Unrecognised directive encountered '" + directive + "'");
+            }
+            
+            // Validate that we're looking at a r-brace.
+            if (!expect(condition(kdl::lexer::token::type::rbrace).truthy())) {
+                throw std::runtime_error("Unexpected token encounted after directive.");
+            }
+            advance();
+        }
+        
+    }
+}
+
+// MARK: - Stream
+
+bool kdl::sema::finished(long offset, long count) const
+{
+    auto ptr = (m_ptr + offset);
+    auto end_ptr = ptr + count;
+    auto size = m_tokens.size();
+    return ptr > size || end_ptr > size;
+}
+
+void kdl::sema::advance(long delta)
+{
+    m_ptr += delta;
+}
+
+kdl::lexer::token kdl::sema::read(long offset)
+{
+    auto tk = peek(offset);
+    m_ptr += offset + 1;
+    return tk;
+}
+
+kdl::lexer::token kdl::sema::peek(long offset) const
+{
+    if (finished(offset, 1)) {
+        throw std::runtime_error("Attempted to access token beyond end of token stream.");
+    }
+    
+    return m_tokens[m_ptr + offset];
+}
+
+std::vector<kdl::lexer::token> kdl::sema::consume(kdl::condition::falsey_function f)
+{
+    std::vector<kdl::lexer::token> v;
+    
+    while (!finished() && f(peek())) {
+        v.push_back(read());
+    }
+    
+    return v;
+}
+
+
+// MARK: - Conditions / Expectations
+
+bool kdl::sema::expect(kdl::condition::truthy_function f) const
+{
+    return !finished() && f(peek());
+}
+
+template <typename ...T>
+bool kdl::sema::expect(kdl::condition::truthy_function f, T... fArgs) const
+{
+    auto ptr = 0;
+    std::vector<kdl::condition::truthy_function> v = { f, fArgs... };
+    for (decltype(v)::iterator i = v.begin(); i != v.end() && !finished(ptr); ++i, ++ptr) {
+        if ((*i)(peek(ptr)) == false) {
+            return false;
+        }
+    }
+    return true;
+}
+
+kdl::condition::condition(kdl::lexer::token::type Ty)
+    : m_Ty(Ty), m_Tx("")
+{
+    
+}
+
+kdl::condition::condition(const std::string Tx)
+    : m_Ty(kdl::lexer::token::type::unknown), m_Tx(Tx)
+{
+    
+}
+
+kdl::condition::condition(kdl::lexer::token::type Ty, const std::string Tx)
+    : m_Ty(Ty), m_Tx(Tx)
+{
+    
+}
+
+kdl::condition::evaluation_function kdl::condition::to_be(bool r)
+{
+    auto& Tx = m_Tx;
+    auto& Ty = m_Ty;
+    
+    return [Tx, Ty, r] (kdl::lexer::token Tk) -> bool {
+        bool outcome = true;
+        
+        if (!Tx.empty() && Tx != Tk.text()) {
+            outcome = false;
+        }
+        
+        if (Ty != kdl::lexer::token::type::unknown && !Tk.is_a(Ty)) {
+            outcome =  false;
+        }
+        
+        return (outcome == r);
+    };
+}
+
+kdl::condition::truthy_function kdl::condition::truthy()
+{
+    return to_be(true);
+}
+
+kdl::condition::falsey_function kdl::condition::falsey()
+{
+    return to_be(false);
 }
--- kas/kdl/sema.hpp
+++ kas/kdl/sema.hpp
@@ -21,6 +21,7 @@
 */
 
 #include <vector>
+#include <string>
 #include "kdl/lexer.hpp"
 
 #if !defined(KDL_SEMA)
@@ -28,6 +29,30 @@
 
 namespace kdl
 {
+
+// MARK: - Expectation Condition Templates
+
+struct condition {
+public:
+    typedef std::function<bool(kdl::lexer::token)> evaluation_function;
+    typedef std::function<bool(kdl::lexer::token)> truthy_function;
+    typedef std::function<bool(kdl::lexer::token)> falsey_function;
+    
+    condition(kdl::lexer::token::type Ty);
+    condition(const std::string Tx);
+    condition(kdl::lexer::token::type Ty, const std::string Tx);
+    
+    evaluation_function to_be(bool r);
+    
+    truthy_function truthy();
+    falsey_function falsey();
+    
+private:
+    kdl::lexer::token::type m_Ty;
+    std::string m_Tx;
+};
+
+// MARK: - Semantic Analysis
 
 /**
  * The `kdl::sema` class is responsible for performing semantic analysis on a
@@ -43,9 +68,54 @@ public:
      */
     sema(const std::vector<kdl::lexer::token> tokens);
     
+    /**
+     * Run/perform semantic analysis on the token stream.
+     */
+    void run();
+    
+    /**
+     * Check if the semantic analysis has reached the end of the token stream.
+     */
+    bool finished(long offset = 0, long count = 1) const;
+   
+    /**
+     * Keep consuming tokens until the expectation is met.
+     *
+     * \return A vector containing all the tokens that were consumed.
+     */
+    std::vector<kdl::lexer::token> consume(kdl::condition::falsey_function f);
+    
+    /**
+     * Advance past the specified number of tokens.
+     */
+    void advance(long delta = 1);
+    
+    /**
+     * Read a token from the token stream.
+     */
+    kdl::lexer::token read(long offset = 0);
+    
+    /**
+     * Peek a token from the token stream.
+     */
+    kdl::lexer::token peek(long offset = 0) const;
+    
+    /**
+     * Validate the expectation of a token.
+     */
+    bool expect(kdl::condition::truthy_function f) const;
+    
+    /**
+     * Validate the expection of a sequence of tokens.
+     */
+    template <typename ...T>
+    bool expect(kdl::condition::truthy_function f, T...fArgs) const;
+    
 private:
+    long m_ptr { 0 };
     std::vector<kdl::lexer::token> m_tokens;
 };
+
 
 };
 
